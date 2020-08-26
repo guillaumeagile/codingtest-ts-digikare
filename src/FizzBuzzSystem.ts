f9b1c378832d2b47fadbc7be33c38bb41adf8181
Guillaume <yohm31@gmail.com>
2020-08-27 00:29:23 +0200

    5nd round, last spec: multiple of 3 and 5 + refactor for the next move

--- src/FizzBuzzSystem.ts
+++ src/FizzBuzzSystem.ts
@@ -2,12 +2,10 @@
 export default class FizzBuzzSystem {
 
   Say(x: number): string {
-    if (x ==0)
-      return "0";  
     
     let output: string = "";
-    let divBy3: boolean = (x % 3 ==0);
-     let divBy5: boolean = (x % 5 ==0)
+    let divBy3: boolean = (x>0) && (x % 3 ==0);
+    let divBy5: boolean =(x>0) && (x % 5 ==0)
     if (divBy3)
       output += "Fizz";
     if (divBy5)
