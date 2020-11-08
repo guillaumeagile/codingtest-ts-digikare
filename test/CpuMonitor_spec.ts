d3ed600092a7ccee86736d5bd512a6c0d1b489b8
guillaume de Touloluse <yohm31@gmail.com>
2020-11-08 14:02:06 +0100

    on peut bien avoir plusieurs imports dans un fichier ts (ici dans le fichier de test)

--- test/CpuMonitor_spec.ts
+++ test/CpuMonitor_spec.ts
@@ -1,4 +1,5 @@
 import Client from "../src/Client";
+import CpuMonitor from "../src/CpuMonitor";
 import {expect} from 'chai';
 
 describe('Cpu Monitor', () => {
@@ -10,10 +11,12 @@ describe('Cpu Monitor', () => {
         it('Should failed', (): void => {
             //ARANGE
             sut = new Client();
+            let cpuMon = new CpuMonitor();
             //ACT
             let result: boolean = sut.AlertService();
             //ASSERT
             expect(result).to.equal(false);
+            expect(CpuMonitor).to.not.be.null;
         });
 
     });
