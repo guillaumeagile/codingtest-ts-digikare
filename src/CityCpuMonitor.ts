481aa269be9781844fe6ae8eb46a6a2093809fbf
acassaigne <anthony.cassaigne@gmail.com>
2020-11-27 17:33:56 +0100

    wip

--- src/CityCpuMonitor.ts
+++ src/CityCpuMonitor.ts
@@ -1,11 +1,27 @@
 import IExposeAlerts from "./IExposeAlerts";
 
+
+async function http(
+    request: RequestInfo
+): Promise<any> {
+    const response = await fetch(request);
+    const body = await response.json();
+    return body;
+}
+
 export default class CityCpuMonitor implements IExposeAlerts {
 
     static readonly DEFAULT_THRESHOLD = 90;
 
+
     TemperatureAlert(): boolean {
         throw new Error("Method not implemented.");
+        const data = await http(
+            "https://jsonplaceholder.typicode.com/todos/71"
+        );
+
+        return data.id > CityCpuMonitor.DEFAULT_THRESHOLD;
+
     }
 
 }
